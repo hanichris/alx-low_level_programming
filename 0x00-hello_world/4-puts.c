792c8ee57d195ace2d7febfe79e7cc5529d01ff7
hanichris <hanichris71@gmail.com>
2022-04-20 11:30:56 -0700

    Using puts() function in c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+	//Initialize the string
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	//Write to stdout
+	puts(str);
+	return 0;
+}
+
