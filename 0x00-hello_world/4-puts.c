5fee8498a873436955131cf816118db85ad6140a
hanichris <hanichris71@gmail.com>
2022-04-20 11:42:17 -0700

    Using puts function in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-	//Initialize the string
+	/*Initialize the string*/
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
-	//Write to stdout
+	/*Write to stdout*/
 	puts(str);
-	return 0;
+	return (0);
 }
 
