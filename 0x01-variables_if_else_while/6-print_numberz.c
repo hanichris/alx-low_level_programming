59be6263a8caab6fbdef32eb3f3664813b99b5aa
hanichris <hanichris71@gmail.com>
2022-04-23 08:35:08 -0700

    print all single digit numbers in base 10 without using char data type

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
+
