41be9d38b7268d02c2e2a6d01021d46a89defc70
hanichris <hanichris71@gmail.com>
2022-04-26 09:43:53 -0700

    submit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - return length of a string
+ * @s : pointer to the character array
+ * Return: String length 
+ */
+int _strlen(char *s)
+{
+	int iter = 0, len = 0;
+
+	while (s[iter] != '\0')
+	{
+		len++;
+		iter++;
+	}
+	return (len);
+}
