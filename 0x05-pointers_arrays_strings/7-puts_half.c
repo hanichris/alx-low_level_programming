3e81371d5ff8f65c62048c94fba2955f73bcdfa4
hanichris <hanichris71@gmail.com>
2022-04-26 11:00:07 -0700

    submit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,11 +6,11 @@
 
 void puts_half(char *str)
 {
-int len = 0;
+int i, len = 0;
 for (len = 0; str[len] != '\0'; len++)
 ;
 
-for (int i = (len + 1) / 2; str[i] != '\0'; i++)
+for (i = (len + 1) / 2; str[i] != '\0'; i++)
 {
 	_putchar(str[i]);
 }
