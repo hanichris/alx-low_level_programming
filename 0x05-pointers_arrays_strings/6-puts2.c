dd1d9e97d93c3f23b7210e4f86a93a2ebb1169e0
hanichris <hanichris71@gmail.com>
2022-04-26 10:46:58 -0700

    submit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+*/
+void puts2(char *str)
+{
+	int iter = 0;
+
+	while (str[iter] != '\0')
+	{
+		_putchar(str[iter]);
+		iter = iter + 2;
+	}
+}
