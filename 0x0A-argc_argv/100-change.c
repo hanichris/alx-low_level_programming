08a12c0ed512bee3f859b7cab808594c2708dca6
hanichris <hanichris71@gmail.com>
2022-05-10 16:32:07 -0700

    compute cents

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compute_min_coin_num(int cents)
+{
+	if (cents <= 0)
+		return (0);
+	else if (cents >= 25)
+		return (1 + compute_min_coin_num(cents - 25));
+	else if (cents >= 10)
+		return (1 + compute_min_coin_num(cents - 10));
+	else if (cents >= 5)
+		return (1 + compute_min_coin_num(cents - 5));
+	else if (cents >= 2)
+		return (1 + compute_min_coin_num(cents - 2));
+	else if (cents % 1 == 0)
+		return (1 + compute_min_coin_num(cents - 1));
+}
+
+/**
+ * main - check code.
+ * @argc : int - argument count.
+ * @argv : char * - argument vector.
+ * Return: 0 or 1.
+ */
+int main(int argc, char *argv[])
+{
+	int cents;
+	int min_coin_num = 0;
+	
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+		printf("%d\n", 0);
+	else
+	{
+		min_coin_num = compute_min_coin_num(cents);
+		printf("%d\n", min_coin_num);
+	}
+
+	return (0);
+}
