993a9846ab497520559d6f4737cecc81f7f7e30f
hanichris <hanichris71@gmail.com>
2022-05-10 17:33:43 -0700

    fix bug

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,23 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stddef.h>
+/**
+ * _calloc - makeshift calloc function.
+ * @nmemb: the number of elements in memory.
+ * @size: the size of the memory to allocate.
+ *
+ * Return: NULL or void pointer.
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *mem;
 	int iter = 0;
-	
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	mem = malloc(nmemb * size);
 	if (!mem)
 		return (NULL);
-	else
+	while (mem[iter])
 	{
-		while (mem[iter])
-		{
-			mem[iter] = 0;
-			iter++;
-		}
-		return ((void *)mem);
+		mem[iter] = 0;
+		iter++;
 	}
+	return ((void *)mem);
+
 }
