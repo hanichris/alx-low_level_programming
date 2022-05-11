855d03d5a6dc1378f99e3f925af514b4d51e4ae4
hanichris <hanichris71@gmail.com>
2022-05-10 17:52:55 -0700

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	mem = malloc(nmemb * size);
 	if (!mem)
 		return (NULL);
-	while (mem[iter])
+	while (iter < (nmemb * size))
 	{
 		mem[iter] = 0;
 		iter++;
