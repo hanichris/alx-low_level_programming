0b93afbdff0a6a58dd454c95acbd18c7946535c5
hanichris <hanichris71@gmail.com>
2022-05-21 16:10:21 -0700

    define the structure of a dog with certain elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef STRUCTURES
+#define STRUCTURES
+/**
+ * struct dog - structure defining a dog.
+ * @name: name of the dog.
+ * @owner: owner of the dog.
+ * @age: age of the dog.
+ *
+ * Description: Properties of the dog include its
+ * name, its owner's name and its age.
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+#endif
