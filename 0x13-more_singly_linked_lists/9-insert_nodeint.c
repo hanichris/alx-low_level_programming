08fee6ad8a8a672ba8355df1fd4f5a9c9915d9fd
hanichris <hanichris71@gmail.com>
2022-06-05 13:43:51 -0700

    submit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new_node->next = current->next;
 			current->next = new_node;
-			return (new_node)
+			return (new_node);
 		}
 		else
 			current = current->next;
