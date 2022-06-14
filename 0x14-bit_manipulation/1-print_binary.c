a026a95e6d3a5bf3b3a426a0df027271d60047b3
hanichris <hanichris71@gmail.com>
2022-06-14 10:53:46 -0700

    print binary representation of a decimal number with variable length

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - print the binary equivalent of a decimal number.
+ * @n: decimal number whose binary equivalent is to be printed out.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+	unsigned long int temp = 0;
+	int i, count_of_1 = 0;
+
+	for (i = 63; i >= 0; i--)
+	{
+		temp = n >> i;
+		if (temp & mask)
+		{
+			_putchar('1');
+			count_of_1++;
+		}
+		else if(count_of_1)
+			_putchar('0');
+	}
+	if (!count_of_1)
+		_putchar('0');
+}
