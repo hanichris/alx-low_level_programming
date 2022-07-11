440d8eb18d7f6eddeebda25081064cba12221ecd
hanichris <hanichris71@gmail.com>
2022-07-11 13:26:09 -0700

    fix prev pointer bug

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,9 +15,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
-	new->n = n;
-	new->next = *head;
-	new->prev = NULL;
-	*head = new;
+	if (!*head)
+	{
+		new->n = n;
+		new->next = *head;
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->n = n;
+		(*head)->prev = new;
+		new->next = *head;
+		new->prev = NULL;
+		*head = new;
+	}
 	return (new);
 }
